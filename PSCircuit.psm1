296eb6964fc07ae0af6825c1d69d56ea7422b295
MaverickSevmont <sevmont@gmail.com>
2020-04-25 16:15:29 -0500

    Add files via upload

---
+++ PSCircuit.psm1
@@ -0,0 +1,35 @@
+﻿
+# Get public and private functions definition files depending on version
+switch ($PSVersionTable.PSEdition) {
+   "Desktop"  {
+        $Public  = @( Get-ChildItem -Path "$PSScriptRoot\PowerShell 5.1\Functions\Public\*.ps1" -ErrorAction SilentlyContinue   )
+        $Private = @( Get-ChildItem -Path "$PSScriptRoot\PowerShell 5.1\Functions\Private\*.ps1" -ErrorAction SilentlyContinue )
+                    ; break}
+   "Core"     {
+        $Public  = @( Get-ChildItem -Path "$PSScriptRoot\PowerShell 7\Functions\Public\*.ps1" -ErrorAction SilentlyContinue   )
+        $Private = @( Get-ChildItem -Path "$PSScriptRoot\PowerShell 7\Functions\Private\*.ps1" -ErrorAction SilentlyContinue  )
+
+                    ; break}
+}
+
+ # Dot source the files
+    Foreach($import in @($Public + $Private))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+#Import Configuration File
+$Config = Import-PowerShellDataFile -LiteralPath "$PSScriptRoot\Config\Config.psd1"
+# Here I might...
+# Read in or create an initial config file and variable
+    # Export Public functions ($Public.BaseName) for WIP modules
+    # Set variables visible to the module and its functions only
+
+Export-ModuleMember -Function $Public.Basename -Variable Config
---
+++ README.md
@@ -0,0 +1,8 @@
+# PSCircuit
+
+Module for Circuit PowerShell + ProdBots.
+
+Documentation is on the way, for questions please contact Erick Sevilla:
+
+marco-polo-erick.sevilla@atos.net
+https://www.linkedin.com/in/ericksevilla/
